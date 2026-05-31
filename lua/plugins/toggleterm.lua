32e449e62c0904facbd313676ecc7c9cd8e73284
Isaito <isaisanchezcc@gmail.com>
2026-05-31 11:10:42 -0600

    Switch toggleterm to vertical split with terminal window navigation keymaps
    Replaces the floating terminal with a vertical split (size 90) for better
    workflow alongside code. Adds terminal-mode keymaps via a TermOpen autocmd:
    jk and <Esc><Esc> to exit terminal mode, <C-h/j/k/l> for window navigation,
    and <C-w> for the window command prefix.

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,14 +3,27 @@ return {
   version = "*",
   config = function()
     require('toggleterm').setup({
-      size = 15,
+      size = 90,
       open_mapping = [[<c-\>]],
-      direction = "float",
-      float_opts = {
-        border = 'rounded',
-        width = 110,
-        height = 20
-      }
+      direction = "vertical",
+    })
+
+    -- Function for setting terminal keymaps
+    local function set_terminal_keymaps()
+      local opts = { buffer = 0 }
+      vim.keymap.set("t", "<esc><esc>", [[<C-\><C-n>]], opts)
+      vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+      vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+      vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+      vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+      vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+      vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+    end
+
+    -- Apply keymaps automatically when a terminal opens
+    vim.api.nvim_create_autocmd("TermOpen", {
+      pattern = "term://*",
+      callback = set_terminal_keymaps,
     })
   end,
 }
