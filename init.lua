2d9310ddaa591702469736e4dd2bcbaca6313745
Isai <60116225+isai7710@users.noreply.github.com>
2026-07-15 22:55:59 -0600

    update to comment text

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 --[[
--- Welcome esteemed traveler to my personal neovim config
+-- Welcome to my personal neovim config
 -- I couldn't have started this config without help from the kickstart.nvim and modular-kickstart.nvim repos, shout out TJ fr, I left a lot of helpful comments from those repositories here for help
 
 10-15 min Lua guide:
@@ -25,8 +25,8 @@ run `:checkhealth` for more info on Neovim health
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
@@ -41,35 +41,35 @@ vim.g.loaded_netrwPlugin = 1
 -- specific module-name, which can be a lua module itself OR a folder with lua submodules
 
 -- [[ Setting options ]]
-require 'options'
+require("options")
 
 -- [[ Basic Keymaps ]]
-require 'keymaps'
+require("keymaps")
 
 -- [[ Basic Autocommands ]]
-require 'autocmds'
+require("autocmds")
 
 --[[ Bootstrap the lazy.nvim plugin manager ]]
 --  NOTE: See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out,                            "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 --[[ Install plugins using lazy ]]
-require('lazy').setup({ { import = "plugins" } }, { change_detection = { notify = false, } })
+require("lazy").setup({ { import = "plugins" } }, { change_detection = { notify = false } })
 
 --  NOTE: To check the current status of your plugins, run the following
 --    :Lazy
